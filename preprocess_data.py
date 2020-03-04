a721e6135ecaaf2c4cde8495c5b0613d7057a83d
Luca <luca.ant96@libero.it>
2020-03-04 20:18:30 +0100

    update

--- preprocess_data.py
+++ preprocess_data.py
@@ -37,7 +37,7 @@ def read_image_as_array(image_path):
 #    cv2.imshow('image', img_o)
 #    k = cv2.waitKey(0)
 
-    return image_array / 255.
+    return image_array * (1. / 255)
 
 
 def load_dataset_from_dir(d):
